342c614df75a4211b941d89c239ae36d9091acf5
Nick Reynolds <nick@joincivil.com>
2023-01-24 13:14:13 -0500

    feat: first commit for veramo react native tutorial

--- App.tsx
+++ App.tsx
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// shims
+import '@sinonjs/text-encoding'
+import 'react-native-get-random-values'
+import '@ethersproject/shims'
+// filename: App.tsx
 
-export default function App() {
+// ... shims
+
+import React, { useEffect, useState } from 'react'
+import { SafeAreaView, ScrollView, View, Text, Button } from 'react-native'
+
+// Import the agent from our earlier setup
+import { agent } from './setup'
+// import some data types:
+import { IIdentifier } from '@veramo/core'
+
+const App = () => {
+  const [identifiers, setIdentifiers] = useState<IIdentifier[]>([])
+
+  // Add the new identifier to state
+  const createIdentifier = async () => {
+    const _id = await agent.didManagerCreate({
+      provider: 'did:ethr:goerli',
+    })
+    setIdentifiers((s) => s.concat([_id]))
+  }
+
+  // Check for existing identifers on load and set them to state
+  useEffect(() => {
+    const getIdentifiers = async () => {
+      const _ids = await agent.didManagerFind()
+      setIdentifiers(_ids)
+
+      // Inspect the id object in your debug tool
+      console.log('_ids:', _ids)
+    }
+
+    getIdentifiers()
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView>
+      <ScrollView>
+        <View style={{ padding: 20 }}>
+          <Text style={{ fontSize: 30, fontWeight: 'bold' }}>Identifiers</Text>
+          <Button onPress={() => createIdentifier()} title={'Create Identifier'} />
+          <View style={{ marginBottom: 50, marginTop: 20 }}>
+            {identifiers && identifiers.length > 0 ? (
+              identifiers.map((id: IIdentifier) => (
+                <View key={id.did}>
+                  <Text>{id.did}</Text>
+                </View>
+              ))
+            ) : (
+              <Text>No identifiers created yet</Text>
+            )}
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
---
+++ setup.ts
@@ -0,0 +1,83 @@
+// filename: setup.ts
+
+// imports:
+// Core interfaces
+import { createAgent, IDataStore, IDataStoreORM, IDIDManager, IKeyManager, IResolver } from '@veramo/core'
+
+// Core identity manager plugin. This allows you to create and manage DIDs by orchestrating different DID provider packages.
+// This implements `IDIDManager`
+import { DIDManager } from '@veramo/did-manager'
+
+// Core key manager plugin. DIDs use keys and this key manager is required to know how to work with them.
+// This implements `IKeyManager`
+import { KeyManager } from '@veramo/key-manager'
+
+// This plugin allows us to create and manage `did:ethr` DIDs. (used by DIDManager)
+import { EthrDIDProvider } from '@veramo/did-provider-ethr'
+
+// A key management system that uses a local database to store keys (used by KeyManager)
+import { KeyManagementSystem, SecretBox } from '@veramo/kms-local'
+
+// Storage plugin using TypeORM to link to a database
+import { Entities, KeyStore, DIDStore, migrations, PrivateKeyStore } from '@veramo/data-store'
+
+// TypeORM is installed with '@veramo/data-store'
+import { DataSource } from 'typeorm'
+
+// filename: setup.ts
+
+// ... imports
+
+// CONSTANTS
+// You will need to get a project ID from infura https://www.infura.io
+const INFURA_PROJECT_ID = '<your PROJECT_ID here>'
+
+// This is a raw X25519 private key, provided as an example.
+// You can run `npx @veramo/cli config create-secret-key` in a terminal to generate a new key.
+// In a production app, this MUST NOT be hardcoded in your source code.
+const DB_ENCRYPTION_KEY = '29739248cad1bd1a0fc4d9b75cd4d2990de535baf5caadfdf8d8f86664aa830c'
+
+// filename: setup.ts
+
+// ... imports & CONSTANTS
+
+// DB setup:
+let dbConnection = new DataSource({
+  type: 'expo',
+  driver: require('expo-sqlite'),
+  database: 'veramo.sqlite',
+  migrations: migrations,
+  migrationsRun: true,
+  logging: ['error', 'info', 'warn'],
+  entities: Entities,
+}).initialize()
+
+// filename: src/veramo/setup.ts
+
+// ... imports & CONSTANTS & DB setup
+
+// Veramo agent setup
+export const agent = createAgent<IDIDManager & IKeyManager & IDataStore & IDataStoreORM>({
+  plugins: [
+    new KeyManager({
+      store: new KeyStore(dbConnection),
+      kms: {
+        local: new KeyManagementSystem(new PrivateKeyStore(dbConnection, new SecretBox(DB_ENCRYPTION_KEY))),
+      },
+    }),
+    new DIDManager({
+      store: new DIDStore(dbConnection),
+      defaultProvider: 'did:ethr:goerli',
+      providers: {
+        'did:ethr:goerli': new EthrDIDProvider({
+          defaultKms: 'local',
+          network: 'goerli',
+          name: 'goerli',
+          rpcUrl: 'https://goerli.infura.io/v3/' + INFURA_PROJECT_ID,
+          gas: 1000001,
+          ttl: 31104001,
+        }),
+      },
+    }),
+  ],
+})
